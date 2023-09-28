5e4a2f29fa319a9cc5233573f8bd9b3a8f609a2d
Jayme Rutkoski <jayme-rutkoski@Jayme-Rutkoski-macpro2021.local>
2023-09-28 11:31:31 -0700

    Add side menu to nav bar

---
+++ frontend/lib/widgets/nav_drawer.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  const NavDrawer({Key? key, this.onSwitchTab}) : super(key: key);
+  final ValueChanged<int>? onSwitchTab;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const SizedBox(
+            height: 100.0,
+            width: double.infinity,
+            child: DrawerHeader(
+              decoration: BoxDecoration(color: Color(0xFF045D5D)),
+              child: Text(
+                'Team Dinner',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.group),
+            title: const Text('Team'),
+            onTap: () => {
+              onSwitchTab?.call(0),
+              Navigator.of(context).pop()
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.poll),
+            title: const Text('Poll'),
+            onTap: () => {
+              onSwitchTab?.call(1),
+              Navigator.of(context).pop()
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text('Profile'),
+            onTap: () => {
+              onSwitchTab?.call(2),
+              Navigator.of(context).pop()
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
