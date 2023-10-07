859f6dc27773b84787b8ab33646bf36da40dec6b
Jayme Rutkoski <jayme-rutkoski@Jayme-Rutkoski-macpro2021.local>
2023-10-06 18:59:51 -0700

    Fix syntax

--- Backend/src/api/api.module.ts
+++ Backend/src/api/api.module.ts
@@ -5,6 +5,6 @@ import { PollsAPIModule } from "./polls/pollsAPI.module";
 import { LocationsAPIModule } from "./locations/locationsAPI.module";
 
 @Module({
-	imports: [TeamsAPIModule, UsersAPIModule, PollsAPIModule, LocationsAPIModule
+	imports: [TeamsAPIModule, UsersAPIModule, PollsAPIModule, LocationsAPIModule]
 })
 export class ApiModule {}
