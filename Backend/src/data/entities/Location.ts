5e47432d2446c10248fba4e47da32a6cdbcd6fc4
Jayme Rutkoski <jayme-rutkoski@Jayme-Rutkoski-macpro2021.local>
2023-10-06 19:35:03 -0700

    Add uuid as a dependency

--- Backend/src/data/entities/Location.ts
+++ Backend/src/data/entities/Location.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty, IsString } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
+import { uuid } from "../../utils/util";
 
 export class Location {
     @ApiProperty()
--- frontend/lib/api/base_repository.dart
+++ frontend/lib/api/base_repository.dart
@@ -1,3 +1,3 @@
 class BaseRepository {
-  static const String baseUrl = "";
+  static const String baseUrl = "https://team-dinner-ono0qk5xj-teamdinner057-gmailcom.vercel.app";
 }
