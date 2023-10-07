7e0822a1b60d63f64d5b6ec13ef66edfb5a99d1b
Jayme Rutkoski <jayme-rutkoski@Jayme-Rutkoski-macpro2021.local>
2023-10-06 19:30:59 -0700

    Add UsersModule to LocationsModule dependency

--- Backend/src/domain/locations/locations.module.ts
+++ Backend/src/domain/locations/locations.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { RepositoryModule } from "../../data/repositories/repository.module";
 import { LocationsService } from "./locations.service";
 import { TeamsModule } from "../teams/teams.module";
+import { UsersModule } from "../users/users.module";
 
 @Module({
-    imports: [RepositoryModule, TeamsModule],
+    imports: [RepositoryModule, TeamsModule, UsersModule],
     exports: [LocationsService],
     providers: [LocationsService]
 })
